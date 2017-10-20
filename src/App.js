0850e5d9a3073af07a15d97e813e957b63ddeb27
Somsak Arnon <lahphim@gmail.com>
2017-10-20 12:25:18 +0700

    Add firebase and update project structure.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,22 @@
 import React, { Component } from 'react';
-import { Router, browserHistory, Route, Link } from 'react-router';
+import {} from 'react-router';
+import { Layout, Header, Content, Drawer } from 'react-mdl';
 import logo from './logo.svg';
 import './App.css';
-
-const Page = ({ title }) => (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>{title}</h2>
-      </div>
-      <p className="App-intro">
-        This is the {title} page.
-      </p>
-      <p>
-        <Link to="/">Home</Link>
-      </p>
-      <p>
-        <Link to="/about">About</Link>
-      </p>
-      <p>
-        <Link to="/settings">Settings</Link>
-      </p>
-    </div>
-);
-
-const Home = (props) => (
-  <Page title="Home"/>
-);
-
-const About = (props) => (
-  <Page title="About"/>
-);
-
-const Settings = (props) => (
-  <Page title="Settings"/>
-);
 
 class App extends Component {
   render() {
     return (
-      <Router history={browserHistory}>
-        <Route path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/settings" component={Settings}/>
-      </Router>
+      <div className="App">
+        <Layout fixedHeader>
+          <Header title="Travelbook" />
+
+          <Drawer />
+          <Content>
+            <img src={logo} className="App-logo" alt="logo" />
+          </Content>
+        </Layout>
+      </div>
     );
   }
 }
